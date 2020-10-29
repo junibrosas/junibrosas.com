3bb7c386e1277084e5f27bd4f8f1a6a74960a48c
Juni Brosas <powerlogic1992@gmail.com>
2020-10-30 00:18:31 +0800

    change profile image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://junibrosas.com/`,
     // Your Name
     name: 'Juni Brosas',
     // Main Site Title
